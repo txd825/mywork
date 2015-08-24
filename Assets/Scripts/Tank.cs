352f412c341f23d454e301c28883a1f322e5b4cf
Your Name <stv1024@gmail.com>
2015-08-24 17:41:06 +0800

    无畏战车
    先上传第2课上完后的状态

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Tank : MonoBehaviour
+{
+
+    public Turret Turret;
+    [SerializeField] private MouseLook _mouseLook;
+
+    void Awake()
+    {
+        _mouseLook.Init(Turret.transform, Turret.MouseLookTra);
+    }
+
+	void Update () {
+
+        _mouseLook.LookRotation(Turret.transform, Turret.MouseLookTra);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 车载武器
+/// </summary>
+public class Turret : MonoBehaviour
+{
+    public Transform MouseLookTra;
+    public Transform FollowLookTra;
+    public GameObject BulletTemplate;
+    public Transform LaunchPoint;
+
+    void Update()
+    {
+        FollowLookTra.localRotation = MouseLookTra.localRotation;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        var go = Instantiate(BulletTemplate);
+        go.transform.position = LaunchPoint.position;
+        go.transform.rotation = LaunchPoint.rotation;
+        go.GetComponent<Rigidbody>().velocity = LaunchPoint.forward*100;
+    }
+}
